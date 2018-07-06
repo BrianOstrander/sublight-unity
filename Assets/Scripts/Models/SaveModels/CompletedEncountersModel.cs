495c2403e9880369963bb4de7dabbc840c0ee892
Brian Ostrander <Ostrander.Brian@gmail.com>
2018-07-06 12:47:10 -0700

    Added encounter models.
    Former-commit-id: 6a4143fa1a10ed21fc5da07a079fc569e56bfebd

---
+++ Assets/Scripts/Models/SaveModels/CompletedEncountersModel.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+using UnityEngine;
+
+namespace LunraGames.SpaceFarm.Models
+{
+	public class CompletedEncountersModel : SaveModel
+	{
+
+
+		public CompletedEncountersModel()
+		{
+			
+		}
+	}
+}
