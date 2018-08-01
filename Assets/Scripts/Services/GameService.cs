5d1e718f6eedd2780a052dc992d7c5454c993014
Brian Ostrander <Ostrander.Brian@gmail.com>
2018-08-01 11:15:30 -0700

    Added a stock engine.
    Former-commit-id: 406065f379acaefc1c3f0e6f01a4b0436de38f09

--- Assets/Scripts/Services/GameService.cs
+++ Assets/Scripts/Services/GameService.cs
@@ -16,6 +16,7 @@ namespace LunraGames.SpaceFarm
 			public const string StockOrbiterBay = "23c48a72-e35a-44d6-bd7e-29e600a76046";
 			public const string StockStorage = "ba230c9e-33a9-4a26-ba2c-de25cc3a0b27";
 			public const string StockLogistics = "90a2c3b4-7b41-449f-b23f-86bc201a1729";
+			public const string StockEngine = "88d5280d-7802-401a-bb25-24b1e4ee46ca";
 			public const string StockTerrestrialOrbiter = "84af3d23-d1c6-4bfd-bec1-c17b11c15269";
 			public const string StockStellarOrbiter = "290702f5-d9a7-43e4-907f-23dfdb579eb9";
 			public const string StockMultiOrbiter = "3442e5dd-00ae-4a1a-bd1c-234e652901d7";
@@ -235,6 +236,20 @@ namespace LunraGames.SpaceFarm
 
 			game.Ship.Value.Inventory.Add(result.Instance);
 
+			App.InventoryReferences.CreateInstance<ModuleInventoryModel>(DefaultShip.StockEngine, instanceResult => OnStockEngineLoaded(instanceResult, game, done));
+		}
+
+		void OnStockEngineLoaded(InventoryReferenceRequest<ModuleInventoryModel> result, GameModel game, Action<RequestStatus, GameModel> done)
+		{
+			if (result.Status != RequestStatus.Success)
+			{
+				Debug.LogError(result.Error);
+				done(result.Status, null);
+				return;
+			}
+
+			game.Ship.Value.Inventory.Add(result.Instance);
+
 			App.InventoryReferences.CreateInstance<OrbitalCrewInventoryModel>(DefaultShip.StockTerrestrialOrbiter, instanceResult => OnStockTerrestrialOrbiterLoaded(instanceResult, game, done));
 		}
 
@@ -288,6 +303,7 @@ namespace LunraGames.SpaceFarm
 			var stockOrbiterBay = inventory.GetInventoryFirstOrDefault<ModuleInventoryModel>(i => i.InventoryId.Value == DefaultShip.StockOrbiterBay);
 			var stockStorage = inventory.GetInventoryFirstOrDefault<ModuleInventoryModel>(i => i.InventoryId.Value == DefaultShip.StockStorage);
 			var stockLogistics = inventory.GetInventoryFirstOrDefault<ModuleInventoryModel>(i => i.InventoryId.Value == DefaultShip.StockLogistics);
+			var stockEngine = inventory.GetInventoryFirstOrDefault<ModuleInventoryModel>(i => i.InventoryId.Value == DefaultShip.StockEngine);
 
 			var stockTerrestrialOrbiter = inventory.GetInventoryFirstOrDefault<OrbitalCrewInventoryModel>(i => i.InventoryId.Value == DefaultShip.StockTerrestrialOrbiter);
 			var stockStellarOrbiter = inventory.GetInventoryFirstOrDefault<OrbitalCrewInventoryModel>(i => i.InventoryId.Value == DefaultShip.StockStellarOrbiter);
@@ -297,6 +313,7 @@ namespace LunraGames.SpaceFarm
 			var rootModule1 = stockRoot.Slots.GetSlotFirstOrDefault<ModuleSlotModel>("module_1");
 			var rootModule2 = stockRoot.Slots.GetSlotFirstOrDefault<ModuleSlotModel>("module_2");
 			var rootModule3 = stockRoot.Slots.GetSlotFirstOrDefault<ModuleSlotModel>("module_3");
+			var rootModule4 = stockRoot.Slots.GetSlotFirstOrDefault<ModuleSlotModel>("module_4");
 
 			var orbitalBayOrbital0 = stockOrbiterBay.Slots.GetSlotFirstOrDefault<ModuleSlotModel>("orbital_0");
 			var orbitalBayOrbital1 = stockOrbiterBay.Slots.GetSlotFirstOrDefault<ModuleSlotModel>("orbital_1");
@@ -304,6 +321,7 @@ namespace LunraGames.SpaceFarm
 			inventory.Connect(rootModule0, stockOrbiterBay);
 			inventory.Connect(rootModule1, stockStorage);
 			inventory.Connect(rootModule2, stockLogistics);
+			inventory.Connect(rootModule3, stockEngine);
 
 			inventory.Connect(orbitalBayOrbital0, stockTerrestrialOrbiter);
 			inventory.Connect(orbitalBayOrbital1, stockStellarOrbiter);
