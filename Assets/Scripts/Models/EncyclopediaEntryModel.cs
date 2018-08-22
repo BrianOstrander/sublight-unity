7613e33e8a57e131f33e1d5824bef0980dd7de5d
Brian Ostrander <Ostrander.Brian@gmail.com>
2018-08-22 11:35:35 -0700

    Added entries and list for encyclopedia.
    Former-commit-id: 77d18f0b7aafa97986dd9b9e606924ba5e3f061c

---
+++ Assets/Scripts/Models/EncyclopediaEntryModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace LunraGames.SubLight.Models
+{
+	public class EncyclopediaEntryModel : Model
+	{
+		[JsonProperty] string encyclopediaId;
+		[JsonProperty] string title;
+		[JsonProperty] string header;
+		[JsonProperty] string message;
+		[JsonProperty] int priority;
+		[JsonProperty] int orderWeight;
+
+		[JsonIgnore]
+		public readonly ListenerProperty<string> EncyclopediaId;
+		[JsonIgnore]
+		public readonly ListenerProperty<string> Title;
+		[JsonIgnore]
+		public readonly ListenerProperty<string> Header;
+		[JsonIgnore]
+		public readonly ListenerProperty<string> Message;
+		[JsonIgnore]
+		public readonly ListenerProperty<int> Priority;
+		[JsonIgnore]
+		public readonly ListenerProperty<int> OrderWeight;
+
+		public EncyclopediaEntryModel()
+		{
+			EncyclopediaId = new ListenerProperty<string>(value => encyclopediaId = value, () => encyclopediaId);
+			Title = new ListenerProperty<string>(value => title = value, () => title);
+			Header = new ListenerProperty<string>(value => header = value, () => header);
+			Message = new ListenerProperty<string>(value => message = value, () => message);
+			Priority = new ListenerProperty<int>(value => priority = value, () => priority);
+			OrderWeight = new ListenerProperty<int>(value => orderWeight = value, () => orderWeight);
+		}
+
+		/// <summary>
+		/// Gets a duplicate of this entry with a unique EncyclopediaId.
+		/// </summary>
+		/// <value>The duplicate.</value>
+		[JsonIgnore]
+		public EncyclopediaEntryModel Duplicate
+		{
+			get
+			{
+				var result = new EncyclopediaEntryModel();
+				result.EncyclopediaId.Value = Guid.NewGuid().ToString();
+				result.Title.Value = Title.Value;
+				result.Header.Value = Header.Value;
+				result.Message.Value = Message.Value;
+				result.Priority.Value = Priority.Value;
+				result.OrderWeight.Value = OrderWeight.Value;
+				return result;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Models/EncyclopediaListModel.cs
@@ -0,0 +1,113 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+
+namespace LunraGames.SubLight.Models
+{
+	public class EncyclopediaListModel : Model
+	{
+		[JsonProperty] EncyclopediaEntryModel[] entries = new EncyclopediaEntryModel[0];
+		[JsonProperty] string[] titles = new string[0];
+
+		[JsonIgnore]
+		readonly ListenerProperty<EncyclopediaEntryModel[]> entriesListener;
+
+		[JsonIgnore]
+		public readonly ReadonlyProperty<EncyclopediaEntryModel[]> Entries;
+		[JsonIgnore]
+		public readonly DerivedProperty<string[], EncyclopediaEntryModel[]> Titles;
+
+		public EncyclopediaListModel()
+		{
+			Entries = new ReadonlyProperty<EncyclopediaEntryModel[]>(
+				value => entries = value,
+				() => entries,
+				out entriesListener
+			);
+
+			Titles = new DerivedProperty<string[], EncyclopediaEntryModel[]>(
+				value => titles = value,
+				() => titles,
+				DeriveTitles,
+				entriesListener
+			);
+		}
+
+		#region Utility
+		public bool HasTitle(string title) { return Entries.Value.Any(e => e.Title.Value == title); }
+		public bool HasHeader(string title, string header) { return Entries.Value.Any(e => e.Title.Value == title && e.Header.Value == header); }
+
+		public int GetHighestOrderWeight(string title)
+		{
+			var highest = Entries.Value.Where(e => e.Title.Value == title).OrderBy(e => e.OrderWeight.Value).LastOrDefault();
+			return highest == null ? 0 : highest.OrderWeight.Value;
+		}
+
+		public EncyclopediaArticleModel GetArticle(string title)
+		{
+			var articleEntries = Entries.Value.Where(e => e.Title.Value == title).OrderBy(e => e.OrderWeight.Value);
+			if (articleEntries.None()) return null;
+
+			var result = new EncyclopediaArticleModel();
+			result.Title.Value = title;
+			result.Entries.Value = articleEntries.ToArray();
+			return result;
+		}
+
+		public EncyclopediaArticleModel[] GetArticles()
+		{
+			var result = new List<EncyclopediaArticleModel>();
+			foreach (var title in Titles.Value) result.Add(GetArticle(title));
+			return result.ToArray();
+		}
+
+		public EncyclopediaEntryModel Get(string title, string header)
+		{
+			return Entries.Value.FirstOrDefault(e => e.Title.Value == title && e.Header.Value == header);
+		}
+
+		public void Add(params EncyclopediaEntryModel[] newEntries)
+		{
+			var entriesToAdd = new List<EncyclopediaEntryModel>();
+			var entriesToRemove = new List<string>();
+
+			foreach (var entry in newEntries)
+			{
+				var existing = Get(entry.Title, entry.Header);
+				if (existing == null)
+				{
+					entriesToAdd.Add(entry);
+					continue;
+				}
+				if (entry.Priority.Value < existing.Priority.Value) continue;
+
+				entry.OrderWeight.Value = existing.OrderWeight.Value;
+
+				entriesToAdd.Add(entry);
+				entriesToRemove.Add(existing.EncyclopediaId.Value);
+				continue;
+			}
+
+			foreach (var entry in entriesToAdd.Where(e => e.OrderWeight.Value == -1))
+			{
+				entry.OrderWeight.Value = GetHighestOrderWeight(entry.Title) + 10;
+			}
+
+			if (entriesToAdd.None()) return;
+
+			foreach (var existing in Entries.Value.Where(e => !entriesToRemove.Contains(e.EncyclopediaId.Value))) entriesToAdd.Add(existing);
+
+			entriesListener.Value = entriesToAdd.ToArray();
+		}
+		#endregion
+
+		#region Events
+		string[] DeriveTitles(EncyclopediaEntryModel[] entries)
+		{
+			var uniqueTitles = entries.Select(e => e.Title.Value).Distinct();
+			return titles.IntersectEqual(uniqueTitles) ? titles : uniqueTitles.ToArray();
+		}
+		#endregion
+	}
+}
