b5ced221f9d2160ce62223b14e954a7c3fbe3b59
Brian Ostrander <Ostrander.Brian@gmail.com>
2018-08-22 09:28:37 -0700

    Made keys for the KeyValue system not case sensitive.
    Former-commit-id: 3451209f3a5911171980998204788e6976c24baa

--- Assets/Scripts/Models/KeyValueListModel.cs
+++ Assets/Scripts/Models/KeyValueListModel.cs
@@ -10,40 +10,48 @@ namespace LunraGames.SubLight.Models
 		[JsonProperty] Dictionary<string, int> integers = new Dictionary<string, int>();
 		[JsonProperty] Dictionary<string, string> strings = new Dictionary<string, string>();
 
+		/// <summary>
+		/// Keys are not case sensitive, so we normalize them here, along with
+		/// any other operations.
+		/// </summary>
+		/// <returns>The key.</returns>
+		/// <param name="key">Key.</param>
+		string NormalizeKey(string key) { return string.IsNullOrEmpty(key) ? key : key.ToLower(); }
+
 		#region Utility
 		public bool GetBoolean(string key, bool fallback = false)
 		{
-			booleans.TryGetValue(key, out fallback);
+			booleans.TryGetValue(NormalizeKey(key), out fallback);
 			return fallback;
 		}
 
 		public int GetInteger(string key, int fallback = 0)
 		{
-			integers.TryGetValue(key, out fallback);
+			integers.TryGetValue(NormalizeKey(key), out fallback);
 			return fallback;
 		}
 
 		public string GetString(string key, string fallback = null)
 		{
-			strings.TryGetValue(key, out fallback);
+			strings.TryGetValue(NormalizeKey(key), out fallback);
 			return fallback;
 		}
 
 		public bool SetBoolean(string key, bool value)
 		{
-			booleans[key] = value;
+			booleans[NormalizeKey(key)] = value;
 			return value;
 		}
 
 		public int SetInteger(string key, int value)
 		{
-			integers[key] = value;
+			integers[NormalizeKey(key)] = value;
 			return value;
 		}
 
 		public string SetString(string key, string value)
 		{
-			strings[key] = value;
+			strings[NormalizeKey(key)] = value;
 			return value;
 		}
 		#endregion
