baf72c2fecc04f3af79460305d552b4b29dc3213
Brian Ostrander <Ostrander.Brian@gmail.com>
2018-12-10 13:10:57 -0800

    Added range info to ship model.

--- Assets/Scripts/Models/ShipModel.cs
+++ Assets/Scripts/Models/ShipModel.cs
@@ -5,23 +5,31 @@ namespace LunraGames.SubLight.Models
 	public class ShipModel : Model
 	{
 		#region Serialized
-		[JsonProperty] InventoryListModel inventory = new InventoryListModel();
 		[JsonProperty] UniversePosition position;
 
 		[JsonIgnore]
-		public InventoryListModel Inventory { get { return inventory; } }
-		[JsonIgnore]
 		public readonly ListenerProperty<UniversePosition> Position;
+
+		[JsonProperty] TravelRange travelRange;
+		[JsonIgnore]
+		ListenerProperty<TravelRange> travelRangeListener;
+		[JsonIgnore]
+		ReadonlyProperty<TravelRange> TravelRange;
+
+		[JsonProperty] InventoryListModel inventory = new InventoryListModel();
+		[JsonIgnore]
+		public InventoryListModel Inventory { get { return inventory; } }
 		#endregion
 
 		public ShipModel()
 		{
 			// Assigned Values
 			Position = new ListenerProperty<UniversePosition>(value => position = value, () => position);
+			TravelRange = new ReadonlyProperty<TravelRange>(value => travelRange = value, () => travelRange, out travelRangeListener);
 		}
 
-		#region Events
-
+		#region Utility
+		public void SetMinimumTravelRange(float minimum) { travelRangeListener.Value = travelRangeListener.Value.NewMinimum(minimum); }
 		#endregion
 	}
 }
--- Assets/Scripts/Services/GameService.cs
+++ Assets/Scripts/Services/GameService.cs
@@ -10,22 +10,15 @@ namespace LunraGames.SubLight
 {
 	public class GameService
 	{
-		static class DefaultGameBlock
+		static class Defaults
 		{
-			public const string GalaxyId = "bed1e465-32ad-4eae-8135-d01eac75a089"; // Milkyway
-			public const string GalaxyTargetId = "a6603c5e-f151-45aa-96bb-30905e781573"; // Andromeda
-		}
+			public static class CreateGameBlock
+			{
+				public const string GalaxyId = "bed1e465-32ad-4eae-8135-d01eac75a089"; // Milkyway
+				public const string GalaxyTargetId = "a6603c5e-f151-45aa-96bb-30905e781573"; // Andromeda
+			}
 
-		static class DefaultShip
-		{
-			//public const string StockRoot = "eccdddb4-553f-4f7a-be7e-b68799839bc8";
-			//public const string StockOrbiterBay = "23c48a72-e35a-44d6-bd7e-29e600a76046";
-			//public const string StockStorage = "ba230c9e-33a9-4a26-ba2c-de25cc3a0b27";
-			//public const string StockLogistics = "90a2c3b4-7b41-449f-b23f-86bc201a1729";
-			//public const string StockEngine = "88d5280d-7802-401a-bb25-24b1e4ee46ca";
-			//public const string StockTerrestrialOrbiter = "84af3d23-d1c6-4bfd-bec1-c17b11c15269";
-			//public const string StockStellarOrbiter = "290702f5-d9a7-43e4-907f-23dfdb579eb9";
-			//public const string StockMultiOrbiter = "3442e5dd-00ae-4a1a-bd1c-234e652901d7";
+			public const float MinimumTravelRange = 0.5f; // In universe units.
 		}
 
 		IModelMediator modelMediator;
@@ -47,15 +40,18 @@ namespace LunraGames.SubLight
 			var game = modelMediator.Create<GameModel>();
 
 			game.Seed.Value = info.GameSeed;
-			game.GalaxyId = StringExtensions.GetNonNullOrEmpty(info.GalaxyId, DefaultGameBlock.GalaxyId);
-			game.GalaxyTargetId = StringExtensions.GetNonNullOrEmpty(info.GalaxyTargetId, DefaultGameBlock.GalaxyTargetId);
+			game.GalaxyId = StringExtensions.GetNonNullOrEmpty(info.GalaxyId, Defaults.CreateGameBlock.GalaxyId);
+			game.GalaxyTargetId = StringExtensions.GetNonNullOrEmpty(info.GalaxyTargetId, Defaults.CreateGameBlock.GalaxyTargetId);
 			game.Universe = universeService.CreateUniverse(info);
 
 			game.FocusTransform.Value = FocusTransform.Default;
 
+			// Ship ---
 			var ship = new ShipModel();
+			ship.SetMinimumTravelRange(Defaults.MinimumTravelRange);
 
 			game.Ship.Value = ship;
+			// --------
 
 			game.ToolbarSelection.Value = ToolbarSelections.System;
 
