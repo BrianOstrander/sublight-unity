839bffe207430a6d37a468e8895019820cfe265f
Brian Ostrander <Ostrander.Brian@gmail.com>
2019-02-19 14:20:57 -0800

    Added population and ration properties to developer grid view.

---
+++ Assets/Scripts/DeveloperStrings.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+
+namespace LunraGames.SubLight
+{
+	public static class DeveloperStrings
+	{
+		public class RatioColor
+		{
+			public Color Minimum;
+			public Color Maximum;
+			public bool Reflect;
+
+			public RatioColor(
+				Color minimum,
+				Color maximum,
+				bool reflect = false
+			)
+			{
+				Minimum = minimum;
+				Maximum = maximum;
+				Reflect = reflect;
+			}
+
+			public Color Evaluate(float normal)
+			{
+				if (Reflect)
+				{
+					if (normal < 0.5f) normal = 1f - (normal / 0.5f);
+					else if (0.5f < normal) normal = (normal - 0.5f) / 0.5f;
+					else normal = 0f;
+				}
+
+				return Color.Lerp(Minimum, Maximum, normal);
+			}
+		}
+
+		public enum RatioThemes
+		{
+			Unknown = 0,
+			Raw = 10,
+			ProgressBar = 20
+		}
+
+		static void RatioShared
+		(
+			float normal,
+			RatioColor color,
+			out string colorTagBegin,
+			out string colorTagEnd
+		)
+		{
+			colorTagBegin = string.Empty;
+			colorTagEnd = string.Empty;
+
+			if (color != null)
+			{
+				colorTagBegin = "<color=#" + ColorUtility.ToHtmlStringRGB(color.Evaluate(normal)) + ">";
+				colorTagEnd = "</color>";
+			}
+		}
+
+		public static string GetRatio(
+			float value,
+			float minimum,
+			float maximum,
+			RatioThemes theme = RatioThemes.Raw,
+			RatioColor color = null,
+			string format = null,
+			int progressBarLimit = 10
+		)
+		{
+			var delta = maximum - minimum;
+			var normal = (value - minimum) / delta;
+			var colorTagBegin = string.Empty;
+			var colorTagEnd = string.Empty;
+
+			RatioShared(
+				normal,
+				color,
+				out colorTagBegin,
+				out colorTagEnd
+			);
+
+			if (string.IsNullOrEmpty(format)) format = "N0";
+
+			switch (theme)
+			{
+				case RatioThemes.Unknown:
+				case RatioThemes.Raw:
+					return colorTagBegin + value.ToString(format) + " / " + maximum.ToString(format) + colorTagEnd;
+				case RatioThemes.ProgressBar:
+					var progressBarResult = colorTagBegin+"|";
+
+					progressBarResult += "==";
+					progressBarLimit -= 2;
+
+					var progressBarHasReachedLimit = false;
+					for (var i = 0; i < progressBarLimit; i++)
+					{
+						var previousLimit = progressBarHasReachedLimit;
+						progressBarHasReachedLimit = Mathf.FloorToInt(progressBarLimit * normal) <= i;
+						if (progressBarHasReachedLimit != previousLimit) progressBarResult += colorTagEnd;
+
+						progressBarResult += "=";
+					}
+					progressBarResult += "|";
+					if (!progressBarHasReachedLimit) progressBarResult += colorTagEnd;
+
+					return progressBarResult;
+				default:
+					Debug.LogError("Unrecognized RatioTheme: " + theme);
+					return string.Empty;
+			}
+		}
+	}
+}
--- Assets/Scripts/Presenters/FocusPresenters/DeveloperFocusPresenters/GridDeveloperPresenter.cs
+++ Assets/Scripts/Presenters/FocusPresenters/DeveloperFocusPresenters/GridDeveloperPresenter.cs
@@ -11,16 +11,68 @@ namespace LunraGames.SubLight.Presenters
 
 		public GridDeveloperPresenter(GameModel model) : base(model)
 		{
+			App.Callbacks.KeyValueRequest += OnKeyValueRequest;
+		}
 
+		protected override void OnUnBind()
+		{
+			base.OnUnBind();
+
+			App.Callbacks.KeyValueRequest -= OnKeyValueRequest;
 		}
 
 		protected override void OnUpdateEnabled()
 		{
-			View.Message = "Lol test";
+			View.Message = CurrentMessage;
+		}
+
+		string CurrentMessage
+		{
+			get
+			{
+				var result = "< Temporary UI >";
+
+				result += "\n";
+
+				var population = Model.KeyValues.GetFloat(DefinedKeyInstances.Game.Population.Key);
+				var populationMinimum = Model.KeyValues.GetFloat(DefinedKeyInstances.Game.PopulationMinimum.Key);
+				var populationMaximum = Model.KeyValues.GetFloat(DefinedKeyInstances.Game.PopulationMaximum.Key);
+
+				result += "Population: " + population.ToString("N0");
+				result += "\n\t" + DeveloperStrings.GetRatio(
+					population,
+					populationMinimum,
+					populationMaximum,
+					DeveloperStrings.RatioThemes.ProgressBar,
+					new DeveloperStrings.RatioColor(Color.green, Color.red, true)
+				);
+
+				result += "\n";
+
+				var rations = Model.KeyValues.GetFloat(DefinedKeyInstances.Game.Rations.Key);
+				var rationsMaximum = Model.KeyValues.GetFloat(DefinedKeyInstances.Game.RationsMaximum.Key);
+
+				result += "Rations: " + rations.ToString("N0");
+				result += "\n\t" + DeveloperStrings.GetRatio(
+					rations,
+					0f,
+					rationsMaximum,
+					DeveloperStrings.RatioThemes.ProgressBar,
+					new DeveloperStrings.RatioColor(Color.red, Color.green)
+				);
+
+				result += "\n";
+				return result;
+			}
 		}
 
 		#region Events
+		void OnKeyValueRequest(KeyValueRequest request)
+		{
+			if (!View.Visible) return;
 
+			View.Message = CurrentMessage;
+		}
 		#endregion
 	}
 }
