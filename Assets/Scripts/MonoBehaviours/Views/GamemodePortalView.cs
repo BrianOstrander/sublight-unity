6fd924ef8d69ec168350e3850afbd90d07feda61
Brian Ostrander <Ostrander.Brian@gmail.com>
2019-03-27 14:16:07 -0700

    Basics of shader and view for gamemode selection.

---
+++ Assets/Scripts/MonoBehaviours/Views/GamemodePortalView.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace LunraGames.SubLight.Views
+{
+	public class GamemodePortalView : View, IGamemodePortalView
+	{
+#pragma warning disable CS0649 // Field is never assigned to, and will always have its default value null
+
+#pragma warning restore CS0649 // Field is never assigned to, and will always have its default value null
+
+		public Color HoloColor { set { } } // lipMesh.material.SetColor(ShaderConstants.HoloLipAdditive.LipColor, value); } }
+
+		public override void Reset()
+		{
+			base.Reset();
+
+			HoloColor = Color.white;
+		}
+	}
+
+	public interface IGamemodePortalView : IView, IHoloColorView
+	{
+
+	}
+}
---
+++ Assets/Scripts/Presenters/GamemodePortalPresenter.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+using LunraGames.SubLight.Views;
+
+namespace LunraGames.SubLight.Presenters
+{
+	public class GamemodePortalPresenter : Presenter<IGamemodePortalView>, IPresenterCloseShowOptions
+	{
+		public GamemodePortalPresenter()
+		{
+			App.Callbacks.HoloColorRequest += OnHoloColorRequest;
+		}
+
+		protected override void OnUnBind()
+		{
+			App.Callbacks.HoloColorRequest -= OnHoloColorRequest;
+		}
+
+		public void Show(Transform parent = null, bool instant = false)
+		{
+			if (View.Visible) return;
+
+			View.Reset();
+
+			View.HoloColor = App.Callbacks.LastHoloColorRequest.Color;
+
+			ShowView(parent, instant);
+		}
+
+		public void Close(bool instant = false)
+		{
+			if (!View.Visible) return;
+
+			CloseView(instant);
+		}
+
+		#region Events
+		void OnHoloColorRequest(HoloColorRequest request)
+		{
+			View.HoloColor = request.Color;
+		}
+		#endregion
+	}
+}
